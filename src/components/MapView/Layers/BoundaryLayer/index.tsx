5f7ce3eaa7616f91ca14f583d13bb0c88d94d376
Eric Boucher <eric.p.boucher@gmail.com>
2022-01-13 15:40:00 -0800

    Test union reducer and others

--- src/components/MapView/Layers/BoundaryLayer/index.tsx
+++ src/components/MapView/Layers/BoundaryLayer/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'lodash';
+import dissolve from '@turf/dissolve';
+import union from '@turf/union';
 import { GeoJSONLayer } from 'react-mapbox-gl';
-import * as MapboxGL from 'mapbox-gl';
+import MapboxGL from 'mapbox-gl';
+// import { Feature, MultiPolygon, Polygon, Properties } from '@turf/helpers';
+import turf from '@turf/turf';
 import { showPopup } from '../../../../context/tooltipStateSlice';
 import { BoundaryLayerProps } from '../../../../config/types';
 import { LayerData } from '../../../../context/layers/layer-data';
 import { layerDataSelector } from '../../../../context/mapStateSlice/selectors';
 import { toggleSelectedBoundary } from '../../../../context/mapSelectionLayerStateSlice';
+import { GeoJsonBoundary } from '../raster-utils';
 
 function onToggleHover(cursor: string, targetMap: MapboxGL.Map) {
   // eslint-disable-next-line no-param-reassign, fp/no-mutation
@@ -37,12 +42,123 @@ function BoundaryLayer({ layer }: { layer: BoundaryLayerProps }) {
     );
   };
 
+  // const layerByAdminLevels = layer.adminLevelNames.map(adminLevelName =>
+  //   dissolve(data, { propertyName: adminLevelName }),
+  // );
+
+  // Group features and merge them by admin level. Here "ST" for testing.
+  const firstLevelData = data.features.reduce(
+    (acc: Record<string, GeoJsonBoundary>, f) => {
+      const feature = f as GeoJsonBoundary;
+      const levelKey: string = get(feature, ['properties', 'ST'], 'no_level');
+      const existingData = get(acc, [levelKey]);
+      // console.log({ levelKey, existingData });
+      try {
+        const newAcc = existingData
+          ? {
+              ...acc,
+              [levelKey]: union(existingData, feature) as GeoJsonBoundary,
+            }
+          : {
+              ...acc,
+              [levelKey]: feature,
+            };
+        return newAcc;
+      } catch {
+        return acc;
+      }
+    },
+    {} as Record<string, GeoJsonBoundary>,
+  );
+
+  const newData = {
+    ...data,
+    features: Object.values(firstLevelData),
+  };
+
+  console.log(firstLevelData);
+
+  // const firstLevelDataLists = data.features.reduce(
+  //   (acc: Record<string, GeoJsonBoundary[]>, f) => {
+  //     const feature = f as GeoJsonBoundary;
+  //     const levelKey: string = get(feature, ['properties', 'ST'], 'no_level');
+  //     const existingData = get(acc, [levelKey]);
+  //     console.log({ levelKey, existingData });
+  //     const newAcc = existingData
+  //       ? {
+  //           ...acc,
+  //           [levelKey]: acc[levelKey].concat(feature),
+  //         }
+  //       : {
+  //           ...acc,
+  //           [levelKey]: [feature],
+  //         };
+  //     return newAcc;
+  //   },
+  //   {} as Record<string, GeoJsonBoundary[]>,
+  // );
+
+  // console.log(firstLevelDataLists);
+
+  // const finalData = Object.keys(firstLevelDataLists).map(key => {
+  //   const groupValues = firstLevelDataLists[key]
+  //   if (groupValues.length >= 2) {
+  //   // eslint-disable-next-line prefer-spread
+  //     return union.apply(null, groupValues)
+  //   }
+  //   return null
+  //   }
+  // );
+
+  // const firstLevelData = data.features.reduce((acc, feature) => {
+  //   const levelKey = get(feature, ['properties', 'ST'], 'no_level');
+  //   const existingData = get(acc, [levelKey]);
+  //   return {
+  //     ...acc,
+  //     [levelKey]: existingData
+  //       ? union(
+  //           existingData,
+  //           feature as Feature<Polygon | MultiPolygon, Properties>,
+  //         )
+  //       : feature,
+  //   };
+  // }, {});
+
+  // console.log(layerByAdminLevels)
+
+  // const layers = Object.keys(rawLayers).reduce(
+  //   (acc, layerKey) => ({
+  //     ...acc,
+  //     [layerKey]: getLayerByKey(layerKey as LayerKey),
+  //   }),
+  //   {} as LayersMap,
+  // );
+
+  // Construct adminLayer line-width case expression.
+  const lineWidthCaseExpression: any[] = ['case'];
+  // TODO - Use reduceRight
+  layer.adminLevelNames
+    .slice()
+    .reverse()
+    .forEach((levelName, index) => {
+      lineWidthCaseExpression.push(['has', levelName], 0.5 + 0.5 * index);
+    });
+  // Push default
+  lineWidthCaseExpression.push(0.5);
+
+  console.log(lineWidthCaseExpression);
+
   return (
     <GeoJSONLayer
       id={layer.id === 'admin_boundaries' ? 'boundaries' : layer.id}
-      data={data}
+      data={newData}
+      // layerOptions={{ filter: ['has', 'TS_PCODE'] }}
       fillPaint={layer.styles.fill}
-      linePaint={layer.styles.line}
+      linePaint={{
+        'line-color': 'gray',
+        'line-width': lineWidthCaseExpression,
+        'line-opacity': 0.8,
+      }}
       fillOnMouseEnter={(evt: any) => onToggleHover('pointer', evt.target)}
       fillOnMouseLeave={(evt: any) => onToggleHover('', evt.target)}
       fillOnClick={layer.id === 'admin_boundaries' ? onClickFunc : undefined}
